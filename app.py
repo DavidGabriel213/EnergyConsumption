6a6c56e894e0a2b08afc27cd1a67639cd8777f10
David Gabriel <gdkimati@gmail.com>
2026-05-11 14:56:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 import os
 
 model=joblib.load('LGBM_model.joblib')
-preprocessor=joblib.load('preprocessor_.joblib')
+preprocessor=joblib.load('Preprocessor.joblib')
 print('model and preprocessor loaded')
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
