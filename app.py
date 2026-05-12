52366e6aef17003cf53fb5b0a0573afe395abe74
David Gabriel <gdkimati@gmail.com>
2026-05-12 09:25:05 +0100

    Enable debug mode and run app on all interfaces
    Added debug mode to Flask app and ensured it runs on all interfaces.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 
 model=joblib.load('LGBM_model.joblib')
 preprocessor=joblib.load('preprocessor/preprocessor_.joblib')
-print('model and preprocessor loaded')
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def myfunc():
